5f182d2f411d124140fcffb247a59ff9d4e60f9f
KoriganGH <kirorg_cool@mail.ru>
2023-07-31 11:31:12 +0300

    formatting

--- PS/views.py
+++ PS/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from PS.models import BasicUser, Role
 from django.views.decorators.csrf import csrf_exempt
 
+
 # Create your views here.
 
 def get_user(request):
@@ -13,31 +14,32 @@ def get_user(request):
         print(i)
     return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
 
+
 @csrf_exempt
 def test(request):
-        print(request.method)
-        new_user = BasicUser()
+    print(request.method)
+    new_user = BasicUser()
 
-        new_user.first_name = request.POST.get('first_name')
-        new_user.surname = request.POST.get('surname')
-        new_user.email = request.POST.get('email')
-        new_user.number = request.POST.get('number')
-        new_user.role = Role.objects.get(id = 1)
-        # new_user.role = Role.objects.filter(RoleName = request.POST.get('role'))[0]
-        new_user.lab = request.POST.get('lab')
-        new_user.permissions = {'sadas': 200}
-        new_user.orders_count = 0
-        new_user.save()
-        return HttpResponse("{status: 200}")
+    new_user.first_name = request.POST.get('first_name')
+    new_user.surname = request.POST.get('surname')
+    new_user.email = request.POST.get('email')
+    new_user.number = request.POST.get('number')
+    new_user.role = Role.objects.get(id=1)
+    # new_user.role = Role.objects.filter(RoleName = request.POST.get('role'))[0]
+    new_user.lab = request.POST.get('lab')
+    new_user.permissions = {'sadas': 200}
+    new_user.orders_count = 0
+    new_user.save()
+    return HttpResponse("{status: 200}")
+
 
 def test2(request):
     reviews = BasicUser.objects.all()
     return reviews[0].login
+
 
 def basic_user_list(request):
     users = BasicUser.objects.all()
     user_list = list(users.values())
 
     return JsonResponse(user_list, safe=False)
-
-
